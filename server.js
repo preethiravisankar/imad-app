f7c63c81746ca3e0654c4b5571abdf80cc098c83
preethiravisankar <preethiravisankar@gmail.com>
2017-08-08 06:34:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function(req,res){
+    counter = counter + 1;
+    req.send(counter.toStrin());
+})
+
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
  var articleName = req.params.articleName;
